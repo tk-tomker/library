3a840d63eb7b689820bab4f16e26e0156382a69f
tiggymathieson <mathieson.acw@oundleschool.org.uk>
2025-01-09 10:31:52 +0000

    change

--- install.php
+++ install.php
@@ -1,9 +1,11 @@
 <?php
-try {
-    $servername = 'localhost';
-    $username = 'root';
-    $password= '';
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Library";
 
+try {
     $conn = new PDO("mysql:host=$servername", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "CREATE DATABASE IF NOT EXISTS Library";
