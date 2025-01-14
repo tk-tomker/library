b568f0e266cbf1529058b887f13244b1257dc782
tk-tomker <tckersey@icloud.com>
2025-01-14 10:40:37 +0000

    asdf

--- book.php
+++ book.php
@@ -1,8 +1,8 @@
-<form action="addbook.php">
+<form action="addbook.php" method ="post" >
   Book ID:<input type="number" name="bookid"><br>
   Author:<input type="text" name="author"><br>
-  No. Of Pages:<input type="text" name="length"><br>
-  Status:<select name="status">
+  No. Of Pages:<input type="n" name="booklength"><br>
+  Status:<select name="bookstatus">
         <option value="in">In Library</option>
         <option value="out">On Loan</option>
     </select>
