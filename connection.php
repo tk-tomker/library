e0fd1028da56685c636a1956142bc69671bbb0f3
tiggymathieson <mathieson.acw@oundleschool.org.uk>
2025-01-14 10:07:53 +0000

    connection

--- connection.php
+++ connection.php
@@ -7,4 +7,5 @@ $dbname = "Library";
 $conn = new PDO("mysql:host=$servername", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE DATABASE IF NOT EXISTS Library";
-$conn->exec($sql);
+echo("connection");
+?>
