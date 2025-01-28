3802024e65877152908c5133d1b24a31132511a0
tk-tomker <tckersey@icloud.com>
2025-01-28 10:30:09 +0000

    asdf

--- mainpage.php
+++ mainpage.php
@@ -14,11 +14,13 @@
 </style>
 
 <body>
+    
     <div class="jumbotron">
     <h1>Bootstrap Tutorial</h1>
     <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive,
     mobile-first projects on the web.</p>
     </div>
     <div class="container">
+        <img src="
     </div>
 </body>
