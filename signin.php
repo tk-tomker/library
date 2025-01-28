ce3d42792251bc5f97395e88d00a02fe177dc650
tk-tomker <tckersey@icloud.com>
2025-01-28 11:34:55 +0000

    asdfsd

--- signin.php
+++ signin.php
@@ -1,18 +1,41 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  
-   <title>Login</title>
-  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In:Oundle School Library</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<form action="loginprocess.php" method= "POST">
- Username:<input type="text" name="Username"><br>
- Password:<input type="password" name="password"><br>
-  <input type="submit" value="Login">
-</form>
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
 
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+      <form action="loginprocess.php" method= "POST">
+         Username:<input type="text" name="Username"><br>
+         Password:<input type="password" name="password"><br>
+         <input type="submit" value="Login" class="btn btn-primary">
+      </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
 
 </body>
 </html>
