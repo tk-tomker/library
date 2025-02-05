b803a16a7edd604450935ca91cbc414fe70b8368
Tom Kersey <tckersey@icloud.com>
2025-02-05 18:29:16 +0000

    sadfsd

--- mainpage.php
+++ mainpage.php
@@ -45,7 +45,30 @@
         ?>
         <br>
         <input type="submit" value="Loan Book">
+        <?php
+        session_start();
 
+        // Assume you have already created a connection to your database
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "tblloans";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        
+        $user_id = $_SESSION['pupilid'];
+        $sql = "INSERT INTO tblloans (pupilid) VALUES ('$pupilid')";
+        if ($conn->query($sql) === TRUE){
+            echo "New record created successfully";
+        } else {
+            echo "Error: ". $sql . "<br>" . $conn->error;
+
+        }
+
+        $conn->close();
     </form>
 </div>
 </body>
