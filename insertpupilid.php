05e2874396a3da0ab7b659857e60485f644dee29
Tom Kersey <tckersey@icloud.com>
2025-02-06 10:19:02 +0000

    more stuff

--- insertpupilid.php
+++ insertpupilid.php
@@ -1,17 +1,9 @@
 <?php
+include_once ("connection.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     session_start();
 
-    // Database connection
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "library";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+   
 
     if (isset($_POST['booktoloan']) && !empty($_POST['booktoloan'])) {
         $bookid = $_POST['booktoloan'];
@@ -27,14 +19,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $pupil = $_SESSION['pupilid'];
         echo "DEBUG: Pupil ID = " . $pupil . "<br>"; // Debugging Line
 
-        $sql = "INSERT INTO tblloans (pupilid, bookid) VALUES (?, ?)";
+        $sql = "INSERT INTO tblloans (pupilid, bookid) VALUES (:pup, :book)";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ii", $pupil, $bookid);  // Ensure bookid & pupilid are integers
+        $stmt->bindParam(':pup', $pupil);
+        $stmt->bindParam(':book', $bookid);
+      
 
         if ($stmt->execute()){
-            $update_sql = "UPDATE tblbook SET bookstatus ='out' WHERE bookid = ?";
+            $update_sql = "UPDATE tblbooks SET bookstatus ='out' WHERE bookid = :bid";
             $update_stmt = $conn->prepare($update_sql);
-            $update_stmt->bind-param("i", $bookid);
+            $update_stmt->bindparam(":bid", $bookid);
 
         }
 
@@ -44,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['message'] = "Error inserting loan: " . $stmt->error;
         }
 
-        $stmt->close();
+       # $stmt->close();
     } else {
         $_SESSION['message'] = "You must be logged in to loan a book.";
     }
